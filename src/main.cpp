5b925a2fb82cb57fbe298403419eb395909d17b9
Tomasz Scislowicz <toomasz@gmail.com>
2023-03-25 18:07:13 +0100

    add sample code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,39 @@
 #include <Arduino.h>
+#include <ThingifyEsp.h>
 
-void setup() {
-  // put your setup code here, to run once:
+ThingifyEsp thing("Esp32SampleThing");
+Node* switchNode;
+bool previousSwitchState = false;
+const int RelayPin = 4;
+const int SwitchPin = 5;
+
+bool OnBoolChanged(void*_, Node *node)
+{
+    digitalWrite(RelayPin, node->Value.AsBool());
+    return true;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+    thing.Initialize();
+    Serial.begin(500000);	
+    thing.AddDiagnostics();
+
+    pinMode(SwitchPin, INPUT);
+    pinMode(RelayPin, OUTPUT);
+
+    switchNode = thing.AddBoolean("Switch")->OnChanged(OnBoolChanged);
+    thing.Start();
+}
+
+void loop()
+{
+    auto switchState = digitalRead(SwitchPin);
+    if(switchState != previousSwitchState)
+    {
+        switchNode->SetValue(NodeValue::Boolean(!switchNode->Value.AsBool()));
+    }
+    previousSwitchState = switchState;
+
+    thing.Loop();
 }
